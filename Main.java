ccb6e68ffad2625efb3e2d833bda13a5688e521c
Curtice Gough <CurticeGough@cfl.rr.com>
2017-11-06 17:04:13 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main{
+
+	public static void main(String []args){
+	
+	// Change the following variables
+	// to the values that are true of
+	// your birthday
+	int year=2003;
+	int month=7;
+	int day=5;
+        
+	int currentYear=2017;
+	int currentMonth=11;
+	int currentDay=13;
+        
+	int age=(currentYear-year);
+        
+	if(currentMonth<month){
+		--age;
+	}
+        
+	System.out.println("Change the values of the variables in Main.java to get the correct age.");
+	System.out.println("You are "+age+" years old.");
+	}
+}
+
