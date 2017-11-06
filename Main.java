b317868d7992665fea94d140ba23c14eab3b0e58
Curtice Gough <CurticeGough@cfl.rr.com>
2017-11-06 17:05:55 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,18 +8,16 @@ public class Main{
 	int year=2003;
 	int month=7;
 	int day=5;
-        
+	
 	int currentYear=2017;
 	int currentMonth=11;
 	int currentDay=13;
-        
+	
 	int age=(currentYear-year);
-        
+	
 	if(currentMonth<month){
 		--age;
 	}
-        
-	System.out.println("Change the values of the variables in Main.java to get the correct age.");
 	System.out.println("You are "+age+" years old.");
 	}
 }
