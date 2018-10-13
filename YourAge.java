f25c8f2440c31577f408e8c6eae28e99bad2f750
Curtice Gough <CurticeGough@cfl.rr.com>
2018-10-13 11:54:44 -0400

    Updated YourAge.java
    YourAge now automatically gets the current date from java.util.Calendar

--- YourAge.java
+++ YourAge.java
@@ -1,40 +1,33 @@
 import java.util.Scanner;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 public class YourAge
 {
 
 	public static void main(String []args)
 	{
 		Scanner in = new Scanner(System.in);
+		Calendar calendar = new GregorianCalendar();
 		int year;
 		int month;
 		int day;
-	
-		int currentYear;
-		int currentMonth;
-		int currentDay;
-		
+
+		int currentYear = calendar.get(Calendar.YEAR);
+		int currentMonth = calendar.get(Calendar.MONTH) + 1;
+		int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
+
 		System.out.print("What year were you born? (YYYY) ");
 		year = in.nextInt();
-		
+
 		System.out.print("What month were you born? (MM) ");
 		month = in.nextInt();
-		
+
 		System.out.print("What day were you born? (DD) ");
 		day = in.nextInt();
-		
 		System.out.println();
-		System.out.print("What year is it? (YYYY) ");
-		currentYear = in.nextInt();
-		
-		System.out.print("What month is it? (MM) ");
-		currentMonth = in.nextInt();
-		
-		System.out.print("What day is it? (DD) ");
-		currentDay = in.nextInt();
-		System.out.println();
-	
+
 		int age = (currentYear - year);
-	
+
 		if(currentMonth < month) {
 		--age;
 		} else if(currentMonth==month) {
@@ -42,9 +35,8 @@ public class YourAge
 				age --;
 			}
 		}
-	
+
 		System.out.println("You are " + age + " years old.");
-	
+
 	}
 }
-
